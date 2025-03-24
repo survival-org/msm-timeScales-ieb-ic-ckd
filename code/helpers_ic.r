4e463b171502742120725bf712af8951f587e675
Wiegrebe <Simon.Wiegrebe@campus.lmu.de>
2025-03-24 12:30:55 +0100

    wrote functions for coverage, rmse and line plot

--- code/helpers_ic.r
+++ code/helpers_ic.r
@@ -10,12 +10,8 @@ library(survival)
 library(mgcv)
 library(pammtools)
 library(flexsurv)
-
-read_file <- function(a_infile, header="auto",sep="auto",fill=FALSE, ...){
-	tblInRaw <- fread(a_infile, header=header, sep=sep)
-	tblIn <- as.data.frame(tblInRaw,fill=fill)
-	return(tblIn)
-}
+library(knitr)
+library(ggplot2)
 
 
 sim_wrapper <- function(
@@ -49,7 +45,7 @@ sim_wrapper <- function(
 }
 
 
-coverage_wrapper_pam <- function(
+wrapper_pam <- function(
   data,
   job,
   instance,
@@ -71,7 +67,7 @@ coverage_wrapper_pam <- function(
     ic_point == "mid" ~ "Surv(time_ic_mid, status_ic) ~ x1 + x2",
     ic_point %in% c("end", "mid_end") ~ "Surv(time_ic_stop, status_ic) ~ x1 + x2",
     ic_point %in% c("true_time", "oracle") ~ "Surv(time, status) ~ x1 + x2",
-    TRUE ~ "NA") %>%
+    TRUE ~ NA_character_) %>%
     as.formula()
 
   ped <- as_ped(
@@ -121,52 +117,60 @@ coverage_wrapper_pam <- function(
   if(ic_point != "mid_end") {
     nd <- nd %>%
       mutate(
-        true_loghazard = eval(parse(text = formula), envir = pick(everything())),
-        true_hazard = exp(true_loghazard),
-        true_cumu = cumsum(intlen * true_hazard),
-        true_surv = exp(-true_cumu)) %>%
+        loghazard_true = eval(parse(text = formula), envir = pick(everything())),
+        hazard_true = exp(loghazard_true),
+        cumu_true = cumsum(intlen * hazard_true),
+        surv_true = exp(-cumu_true)) %>%
       add_hazard(mod, se_mult = qnorm(0.975), ci_type = "default", type = "link") %>%
       rename(loghazard = hazard, loghazard_lower = ci_lower, loghazard_upper = ci_upper) %>%
       add_hazard(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
       rename(hazard_lower = ci_lower, hazard_upper = ci_upper) %>%
       add_cumu_hazard(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
+      rename(cumu = cumu_hazard) %>%
       add_surv_prob(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
+      rename(surv = surv_prob) %>%
       mutate(
-        loghazard_cov = (true_loghazard >= loghazard_lower) & (true_loghazard <= loghazard_upper),
-        hazard_cov = (true_hazard >= hazard_lower) & (true_hazard <= hazard_upper),
-        cumu_cov = (true_cumu >= cumu_lower) & (true_cumu <= cumu_upper),
-        surv_cov =  (true_surv >= surv_lower) & (true_surv <= surv_upper))
+        loghazard_cov = as.integer((loghazard_true >= loghazard_lower) & (loghazard_true <= loghazard_upper)),
+        hazard_cov = as.integer((hazard_true >= hazard_lower) & (hazard_true <= hazard_upper)),
+        cumu_cov = as.integer((cumu_true >= cumu_lower) & (cumu_true <= cumu_upper)),
+        surv_cov =  as.integer((surv_true >= surv_lower) & (surv_true <= surv_upper)))
   } else {
     nd <- nd %>%
       mutate(tmid = (tstart + tend) / 2) %>%
       mutate(
-        true_loghazard = eval(parse(text = formula), envir = pick(everything())),
-        true_hazard = exp(true_loghazard),
-        true_cumu = cumsum(intlen * true_hazard),
-        true_surv = exp(-true_cumu)) %>%
+        loghazard_true = eval(parse(text = formula), envir = pick(everything())),
+        hazard_true = exp(loghazard_true),
+        cumu_true = cumsum(intlen * hazard_true),
+        surv_true = exp(-cumu_true)) %>%
       rename(tend_original = tend, tend = tmid) %>%
       add_hazard(mod, se_mult = qnorm(0.975), ci_type = "default", type = "link") %>%
       rename(loghazard = hazard, loghazard_lower = ci_lower, loghazard_upper = ci_upper) %>%
       add_hazard(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
       rename(hazard_lower = ci_lower, hazard_upper = ci_upper) %>%
       add_cumu_hazard(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
+      rename(cumu = cumu_hazard) %>%
       add_surv_prob(mod, se_mult = qnorm(0.975), ci_type = "default") %>%
+      rename(surv = surv_prob) %>%
       rename(tmid = tend, tend = tend_original) %>%
       mutate(
-        loghazard_cov = (true_loghazard >= loghazard_lower) & (true_loghazard <= loghazard_upper),
-        hazard_cov = (true_hazard >= hazard_lower) & (true_hazard <= hazard_upper),
-        cumu_cov = (true_cumu >= cumu_lower) & (true_cumu <= cumu_upper),
-        surv_cov =  (true_surv >= surv_lower) & (true_surv <= surv_upper))
+        loghazard_cov = as.integer((loghazard_true >= loghazard_lower) & (loghazard_true <= loghazard_upper)),
+        hazard_cov = as.integer((hazard_true >= hazard_lower) & (hazard_true <= hazard_upper)),
+        cumu_cov = as.integer((cumu_true >= cumu_lower) & (cumu_true <= cumu_upper)),
+        surv_cov =  as.integer((surv_true >= surv_lower) & (surv_true <= surv_upper)))
   }
 
   nd <- nd %>%
-    select(tstart, tend, loghazard, hazard, cumu, surv, loghazard_cov, hazard_cov, cumu_cov, surv_cov)
+    select(
+      tstart, tend,
+      loghazard, hazard, cumu, surv,
+      loghazard_true, hazard_true, cumu_true, surv_true,
+      loghazard_cov, hazard_cov, cumu_cov, surv_cov)
 
   return(nd)
 }
 
 
-coverage_wrapper_cox <- function(
+wrapper_cox <- function(
   data,
   job,
   instance,
@@ -187,7 +191,7 @@ coverage_wrapper_cox <- function(
     ic_point == "mid" ~ "Surv(time_ic_mid, status_ic) ~ x1 + x2",
     ic_point == "end" ~ "Surv(time_ic_stop, status_ic) ~ x1 + x2",
     ic_point %in% c("true_time", "oracle") ~ "Surv(time, status) ~ x1 + x2",
-    TRUE ~ "NA") %>%
+    TRUE ~ NA_character_) %>%
     as.formula()
 
   ped <- as_ped(
@@ -209,26 +213,27 @@ coverage_wrapper_cox <- function(
     x1 = median(df$x1),
     x2 = mean(df$x2)
   ) %>%
-  mutate(
-    true_hazard = exp(eval(parse(text = formula), envir = pick(everything()))),
-    true_cumu = cumsum(intlen * true_hazard),
-    true_surv = exp(-true_cumu)
-  )
+    mutate(
+      loghazard_true = eval(parse(text = formula), envir = pick(everything())),
+      hazard_true = exp(loghazard_true),
+      cumu_true = cumsum(intlen * hazard_true),
+      surv_true = exp(-cumu_true)
+    )
 
   # get baseline estimates and CI via survfit
   newdata_cox <- data.frame(x1=median(df$x1), x2=mean(df$x2))
   sf <- survfit(mod, newdata = newdata_cox)
 
   # Extract baseline values at matching times
-  est_surv <- approx(sf$time, sf$surv, nd$tend, rule=2)$y
+  surv <- approx(sf$time, sf$surv, nd$tend, rule=2)$y
   surv_upper <- approx(sf$time, sf$upper, nd$tend, rule=2)$y
   surv_lower <- approx(sf$time, sf$lower, nd$tend, rule=2)$y
 
-  est_cumu <- -log(est_surv)
+  cumu <- -log(surv)
   cumu_upper <- -log(surv_lower)
   cumu_lower <- -log(surv_upper)
 
-  baseline_hazard <- c(est_cumu[1]/nd$tend[1], diff(est_cumu)/diff(nd$tend))
+  baseline_hazard <- c(cumu[1]/nd$tend[1], diff(cumu)/diff(nd$tend))
 
   # Linear predictor and its SE
   lp_pred <- predict(mod, newdata = newdata_cox, type="lp", se.fit=TRUE)
@@ -238,10 +243,13 @@ coverage_wrapper_cox <- function(
   # hazard, cumu hazard, and survival adjusted by linear predictor
   nd <- nd %>%
     mutate(
-      est_hazard = baseline_hazard * exp(lp), # BUG: no uncertainty in baseline_hazard!!!
-      est_cumu = est_cumu,
-      est_surv = est_surv,
+      loghazard = NA_real_,
+      hazard = baseline_hazard * exp(lp), # BUG: no uncertainty in baseline_hazard!!!
+      cumu = cumu,
+      surv = surv,
       # approximate hazard CI by propagating lp_se (CI on LP)
+      loghazard_lower = NA_real_,
+      loghazard_upper = NA_real_,
       hazard_upper = baseline_hazard * exp(lp + 1.96*lp_se),
       hazard_lower = baseline_hazard * exp(lp - 1.96*lp_se),
       cumu_upper = cumu_upper,
@@ -250,40 +258,40 @@ coverage_wrapper_cox <- function(
       surv_lower = surv_lower
     ) %>%
     mutate(
-      hazard = (true_hazard >= hazard_lower) & (true_hazard <= hazard_upper),
-      cumu = (true_cumu >= cumu_lower) & (true_cumu <= cumu_upper),
-      surv = (true_surv >= surv_lower) & (true_surv <= surv_upper)
-    ) %>%
-    select(tstart, tend, hazard, cumu, surv)
+      loghazard_cov = NA_real_,
+      hazard_cov = as.integer((hazard_true >= hazard_lower) & (hazard_true <= hazard_upper)),
+      cumu_cov = as.integer((cumu_true >= cumu_lower) & (cumu_true <= cumu_upper)),
+      surv_cov =  as.integer((surv_true >= surv_lower) & (surv_true <= surv_upper)))
+
+  nd <- nd %>%
+    select(
+      tstart, tend,
+      loghazard, hazard, cumu, surv,
+      loghazard_true, hazard_true, cumu_true, surv_true,
+      loghazard_cov, hazard_cov, cumu_cov, surv_cov)
 
   return(nd)
 }
 
 
-coverage_wrapper_generalizedGamma <- function(
+wrapper_generalizedGamma <- function(
   data,
   job,
   instance,
-  ic_point = c("mid", "end", "true_time", "oracle"),
-  ic_adjustment = FALSE) {
+  ic_point = c("mid", "end", "true_time", "oracle", "adjustment")) {
 
   ic_point <- match.arg(ic_point)
-
-  if(ic_adjustment) {
-    ic_point <- "mid"
-  }
-
+saveRDS(instance, "instance.rds")
   df <- instance[[1]]
   formula <- instance[[2]]
 
-  # Prepare data based on ic_point unless ic_adjustment = TRUE
   if(ic_point == "mid") {
     df <- df %>% mutate(time_ic_mid = (time_ic_start + time_ic_stop) / 2)
   }
 
   formula_ped <- case_when(
     ic_point == "mid" ~ "Surv(time_ic_mid, status_ic) ~ x1 + x2",
-    ic_point %in% c("end") ~ "Surv(time_ic_stop, status_ic) ~ x1 + x2",
+    ic_point %in% c("end", "adjustment") ~ "Surv(time_ic_stop, status_ic) ~ x1 + x2",
     ic_point %in% c("true_time", "oracle") ~ "Surv(time, status) ~ x1 + x2",
     TRUE ~ NA_character_
   ) %>% as.formula()
@@ -293,7 +301,8 @@ coverage_wrapper_generalizedGamma <- function(
     formula = formula_ped,
     id = "id")
 
-  if(!ic_adjustment){
+  if(ic_point != "adjustment") {
+    # regular formula
     formula_mod <- update(formula_ped, . ~ x1 + pspline(x2))
   }  else {
     # interval-censored formula
@@ -301,19 +310,39 @@ coverage_wrapper_generalizedGamma <- function(
   }
 
   # Model fitting
-  mod <- flexsurvreg(formula_mod, data = df, dist = "gengamma")
+  mod <- tryCatch(
+    withCallingHandlers(
+      {
+        flexsurvreg(formula_mod, data = df, dist = "gengamma",
+                    inits = c(mu = log(median(df$time)), sigma = 0.5, Q = 1),
+                    method = "BFGS")
+      },
+      warning = function(w) {
+        message("Warning detected in BFGS: Falling back to Nelder-Mead")
+        invokeRestart("muffleWarning")  # Prevents warning from propagating further
+      }
+    ),
+    error = function(e) {
+      message("Error in BFGS: Falling back to Nelder-Mead")
+      flexsurvreg(formula_mod, data = df, dist = "gengamma",
+                  inits = c(mu = log(median(df$time)), sigma = 0.5, Q = 1),
+                  method = "Nelder-Mead")
+    }
+  )
 
   # new data preparation
+  formula <- paste(gsub("\\bt\\b", "tend", deparse(formula[[2]])))
+
   nd <- make_newdata(
     ped,
     tend = sort(unique(ped$tend)),
     x1 = median(df$x1),
     x2 = mean(df$x2)) %>%
     mutate(
-      true_hazard = exp(eval(parse(text = paste(gsub("\\bt\\b", "tend", deparse(formula[[2]])))),
-                             envir = pick(everything()))),
-      true_cumu = cumsum(intlen * true_hazard),
-      true_surv = exp(-true_cumu)
+      loghazard_true = eval(parse(text = formula), envir = pick(everything())),
+      hazard_true = exp(loghazard_true),
+      cumu_true = cumsum(intlen * hazard_true),
+      surv_true = exp(-cumu_true)
     )
 
   # predictions from parametric model
@@ -322,22 +351,31 @@ coverage_wrapper_generalizedGamma <- function(
 
   nd <- nd %>%
     mutate(
-      est_hazard = pred[[1]]$est,
+      loghazard = log(pred[[1]]$est),
+      loghazard_lower = log(pred[[1]]$lcl),
+      loghazard_upper = log(pred[[1]]$ucl),
+      hazard = pred[[1]]$est,
       hazard_lower = pred[[1]]$lcl,
       hazard_upper = pred[[1]]$ucl,
-      est_cumu = cumsum(est_hazard * intlen),
+      cumu = cumsum(hazard * intlen),
       cumu_lower = cumsum(hazard_lower * intlen),
       cumu_upper = cumsum(hazard_upper * intlen),
-      est_surv = exp(-est_cumu),
+      surv = exp(-cumu),
       surv_lower = exp(-cumu_upper),
       surv_upper = exp(-cumu_lower)
     ) %>%
     mutate(
-      hazard = (true_hazard >= hazard_lower) & (true_hazard <= hazard_upper),
-      cumu = (true_cumu >= cumu_lower) & (true_cumu <= cumu_upper),
-      surv = (true_surv >= surv_lower) & (true_surv <= surv_upper)
-    ) %>%
-    select(tstart, tend, hazard, cumu, surv)
+      loghazard_cov = as.integer((loghazard_true >= loghazard_lower) & (loghazard_true <= loghazard_upper)),
+      hazard_cov = as.integer((hazard_true >= hazard_lower) & (hazard_true <= hazard_upper)),
+      cumu_cov = as.integer((cumu_true >= cumu_lower) & (cumu_true <= cumu_upper)),
+      surv_cov =  as.integer((surv_true >= surv_lower) & (surv_true <= surv_upper)))
+
+  nd <- nd %>%
+    select(
+      tstart, tend,
+      loghazard, hazard, cumu, surv,
+      loghazard_true, hazard_true, cumu_true, surv_true,
+      loghazard_cov, hazard_cov, cumu_cov, surv_cov)
 
   return(nd)
 }
@@ -459,3 +497,124 @@ add_interval_censoring <- function(
   return(final_data)
 }
 
+
+calc_coverage <- function(data, grouping_vars = NULL, rounding = 3) {
+
+  required_cols <- c(
+    grouping_vars,
+    "loghazard_cov", "hazard_cov", "cumu_cov", "surv_cov"
+  )
+
+  if (!all(required_cols %in% colnames(data))) {
+    stop("Data is missing required columns.")
+  }
+
+  jobLevel_results <- data %>%
+    {if(!is.null(grouping_vars))
+        group_by(., job.id, across(all_of(grouping_vars)))
+    else
+        group_by(., job.id)} %>%
+    summarise(
+      loghazard_cov = mean(loghazard_cov, na.rm = TRUE),
+      hazard_cov = mean(hazard_cov, na.rm = TRUE),
+      cumu_cov = mean(cumu_cov, na.rm = TRUE),
+      surv_cov = mean(surv_cov, na.rm = TRUE)
+    )
+
+  results <- jobLevel_results %>%
+    {if (!is.null(grouping_vars)) group_by(., across(all_of(grouping_vars))) else . } %>%
+    summarise(
+      `coverage loghazard` = round(mean(loghazard_cov, na.rm = TRUE), rounding),
+      `coverage hazard` = round(mean(hazard_cov, na.rm = TRUE), rounding),
+      `coverage cumulative hazard` = round(mean(cumu_cov, na.rm = TRUE), rounding),
+      `coverage survival probability` = round(mean(surv_cov, na.rm = TRUE), rounding),
+      .groups = "drop"
+    )
+
+  return(kable(results))
+}
+
+
+calc_rmse <- function(data, grouping_vars = NULL, rounding = 3) {
+
+  required_cols <- c(
+    grouping_vars,
+    "loghazard", "hazard", "cumu", "surv",
+    "loghazard_true", "hazard_true", "cumu_true", "surv_true"
+  )
+
+  if (!all(required_cols %in% colnames(data))) {
+    stop("Data is missing required columns.")
+  }
+
+  jobLevel_results <- data %>%
+    mutate(
+      loghazard_sq_error = (loghazard_true - loghazard)**2,
+      hazard_sq_error = (hazard_true - hazard)**2,
+      cumu_sq_error = (cumu_true - cumu)**2,
+      surv_sq_error = (surv_true - surv)**2) %>%
+    {if(!is.null(grouping_vars))
+        group_by(., job.id, across(all_of(grouping_vars)))
+    else
+        group_by(., job.id)} %>%
+    summarise(
+      loghazard_rmse = sqrt(mean(loghazard_sq_error)),
+      hazard_rmse = sqrt(mean(hazard_sq_error)),
+      cumu_rmse = sqrt(mean(cumu_sq_error)),
+      surv_rmse = sqrt(mean(surv_sq_error))
+    )
+
+  results <- jobLevel_results %>%
+    { if (!is.null(grouping_vars)) group_by(., across(all_of(grouping_vars))) else . } %>%
+    summarise(
+      `RMSE loghazard` = mean(loghazard_rmse),
+      `RMSE hazard` = mean(hazard_rmse),
+      `RMSE cumulative hazard` = mean(cumu_rmse),
+      `RMSE survival probability` = mean(surv_rmse),
+      .groups = "drop"
+    )
+
+  return(kable(results))
+}
+
+
+create_linePlot <- function(data, grouping_vars = NULL) {
+
+  required_cols <- c(
+    grouping_vars, "tend", "loghazard", "loghazard_true", "job.id"
+  )
+
+  if (!all(required_cols %in% colnames(data))) {
+    stop("Data is missing required columns.")
+  }
+
+  plot_data <- data %>%
+    filter(!is.na(loghazard))
+
+  if (!is.null(grouping_vars)) {
+    plot_data <- plot_data %>%
+      unite("grouping", all_of(grouping_vars), remove = FALSE)
+  } else {
+    plot_data$grouping <- "All"
+  }
+
+  unique_groups <- unique(plot_data$grouping)
+
+  plots <- lapply(unique_groups, function(grp) {
+    df_grp <- plot_data %>% filter(grouping == grp)
+
+    ggplot(df_grp, aes(x = tend, y = loghazard)) +
+      geom_step(aes(group = job.id), alpha = 0.3) +
+      geom_line(aes(y = loghazard_true, col = "truth"), lwd = 1.5) +
+      geom_smooth(aes(col = "average estimate"), method = "gam", formula = y ~ s(x), se = FALSE) +
+      scale_color_brewer("", palette = "Dark2") +
+      xlab("time") +
+      ylab("loghazard") +
+      ggtitle(paste("Group:", grp)) +
+      theme_bw()
+  })
+
+  names(plots) <- unique_groups
+
+  return(plots)
+}
--- code/simulations_ic.r
+++ code/simulations_ic.r
@@ -10,30 +10,35 @@ setwd("C:/Users/ra56yaf/Desktop/Projects/StaBLab/Survival Analysis/survival_kidn
 source("code/helpers_ic.r")
 
 registry_coverage <- "results/sim-coverage-registry"
-repls <- 100
+repls <- 2
 
-set.seed(11022022)
-a <- sim_wrapper(data = NULL, job = NULL, n = 500, time_grid = seq(0, 10, by = 0.05), ic = TRUE, ic_mechanism = "uniform", round = 2)
-b <- coverage_wrapper_pam(data = a, job = NULL, instance = a, bs = "ps", k = 10, ic_point = "mid_end")
-c <- coverage_wrapper_cox(data = a, job = NULL, instance = a, ic_point = "true_time")
-d <- coverage_wrapper_generalizedGamma(data = a, job = NULL, instance = a, ic_point = "end", ic_adjustment = TRUE)
-# b
-# tbd: always compare to oracle pam (using t_true) and Cox and Cox_intervalCens (except for cumu hazard and surv prob?)
+# set.seed(11022022)
+# a <- sim_wrapper(data = NULL, job = NULL, n = 500, time_grid = seq(0, 10, by = 0.05), ic = TRUE, ic_mechanism = "uniform", round = 2)
+# b <- coverage_wrapper_pam(data = a, job = NULL, instance = a, bs = "ps", k = 10, ic_point = "mid_end")
+# c <- coverage_wrapper_cox(data = a, job = NULL, instance = a, ic_point = "true_time")
+# d <- coverage_wrapper_generalizedGamma(data = a, job = NULL, instance = a, ic_point = "end")
+
 
 # coverage overall hazards ----
-# if (checkmate::test_directory_exists(registry_coverage)) {
-#   unlink(registry_coverage, recursive = TRUE)
-# }
+if (checkmate::test_directory_exists(registry_coverage)) {
+  unlink(registry_coverage, recursive = TRUE)
+}
 if (!checkmate::test_directory_exists(registry_coverage)) {
   reg <- makeExperimentRegistry(
     registry_coverage,
     packages = c("mgcv", "dplyr", "tidyr", "pammtools", "mvtnorm", "rlang"),
     seed     = 11022022)
   reg <- loadRegistry(registry_coverage, writeable = TRUE)
-  reg$cluster.functions = makeClusterFunctionsInteractive()
+
+  tryCatch({
+    reg$cluster.functions = makeClusterFunctionsMulticore(ncpus = 150)
+  }, error = function(e) {
+    reg$cluster.functions = makeClusterFunctionsInteractive()
+  })
   addProblem(name = "coverage", fun = sim_wrapper)
-  addAlgorithm(name = "coverage_pam", fun = coverage_wrapper_pam)
-  addAlgorithm(name = "coverage_cox", fun = coverage_wrapper_cox)
+  addAlgorithm(name = "pam", fun = wrapper_pam)
+  addAlgorithm(name = "cox", fun = wrapper_cox)
+  addAlgorithm(name = "generalizedGamma", fun = wrapper_generalizedGamma)
 
   prob_df <- data.frame(
     formula = "~ -3.5 + dgamma(t, 8, 2) * 6 - 1.3 * x1 + sqrt(x2)",
@@ -41,40 +46,46 @@ if (!checkmate::test_directory_exists(registry_coverage)) {
     ic_mechanism = c("beta", "uniform")
   )
   algo_df_pam <- data.frame(
-    ic_point = c("mid", "end", "true_time", "oracle")
+    ic_point = c("mid", "end", "true_time", "oracle", "mid_end")
   )
   algo_df_cox <- data.frame(
     ic_point = c("mid", "end", "true_time", "oracle")
   )
+  algo_df_generalizedGamma <- data.frame(
+    ic_point = c("mid", "end", "true_time", "oracle", "adjustment")
+  )
 
   addExperiments(
     prob.designs = list(coverage = prob_df),
-    algo.designs  = list(coverage_pam = algo_df_pam, coverage_cox = algo_df_cox),
-    # algo.designs  = list(coverage_pam = algo_df_pam),
+    algo.designs  = list(
+      pam = algo_df_pam,
+      cox = algo_df_cox,
+      generalizedGamma = algo_df_generalizedGamma),
     repls = repls)
 
   submitJobs(findNotDone())
-  # waitForJobs()
+     # waitForJobs()
 }
 
 reg     <- loadRegistry(registry_coverage, writeable = TRUE)
-ids_res <- findExperiments(prob.name = "coverage", algo.pattern = "coverage_")
+ids_res <- findExperiments(prob.name = "coverage")
 pars    <- unwrap(getJobPars()) %>% as_tibble()
 res     <- reduceResultsDataTable(ids=findDone(ids_res)) %>%
   as_tibble() %>%
   tidyr::unnest(cols = c(result)) %>%
-  left_join(pars)
+  left_join(pars, by = "job.id")
 
-# RMSE and coverage hazard
-res %>%
-  group_by(ic_point, algorithm, ic_mechanism) %>%
-  summarize(
-    "coverage hazard" = mean(hazard),
-    "coverage cumulative hazard" = mean(cumu),
-    "coverage survival probability" = mean(surv)) %>%
-  ungroup() %>%
-  mutate_if(is.numeric, ~round(., 3)) %>%
-  knitr::kable()
+# coverage overall ----
+coverage <- calc_coverage(data = res, grouping_vars = c("ic_point", "algorithm", "ic_mechanism"))
+coverage
+
+# RMSE ----
+rmse <- calc_rmse(data = res, grouping_vars = c("ic_point", "algorithm", "ic_mechanism"))
+rmse
+
+# line plot ----
+linePlot <- create_linePlot(data = res, grouping_vars = c("ic_point", "algorithm", "ic_mechanism"))
+linePlot
 
 # recovery baseline hazard ----
 ## TBD!!!
@@ -84,93 +95,3 @@ res %>%
 
 # recovery specific effects (non-linear) ----
 ## TBD!!!
-
-# pem vs. pam ----
-
-## estimation function
-pam_wrapper <- function(data, job, instance,
-  cut      = NA,
-  bs       = "ps",
-  mod_type = c("pem", "pam") ,
-  max_time = 10) {
-
-  if(is.na(cut)) {
-    cut <- NULL
-  } else {
-    if(cut == "rough") {
-      cut <- seq(0, max_time, by = 0.5)
-    } else {
-      if(cut == "fine") {
-        cut <- seq(0, max_time, by = 0.2)
-      }
-    }
-  }
-
-  ped <- as_ped(data = instance, formula = Surv(time, status) ~ ., cut = cut, id="id")
-
-  form <- "ped_status ~ s(tend) + s(x1) + s(x2)"
-  if(mod_type == "pem") {
-    form     <- ped_status ~ interval
-    time_var <- "interval"
-  } else {
-    form     <- ped_status ~ s(tend, bs = bs, k = 10)
-    time_var <- "tend"
-  }
-
-  mod <- gam(formula = form, data = ped, family = poisson(), offset = offset, method = "REML")
-  # summary(mod)
-
-  make_newdata(ped, tend=unique(tend)) %>%
-    add_hazard(mod, type="link", se_mult = qnorm(0.975), time_var = time_var) %>%
-    mutate(truth = -3.5 + dgamma(tend, 8, 2) * 6)
-
-}
-
-if(!checkmate::test_directory_exists("C:/Users/ra56yaf/Desktop/Projects/StaBLab/Survival Analysis/survival_kidneyFunction/msm_kidneyFunction/output/sim-pem-vs-pam-registry")) {
-  reg <- makeExperimentRegistry("C:/Users/ra56yaf/Desktop/Projects/StaBLab/Survival Analysis/survival_kidneyFunction/msm_kidneyFunction/output/sim-pem-vs-pam-registry",
-    packages = c("mgcv", "dplyr", "tidyr", "pammtools"),
-    seed     = 20052018)
-  # reg$cluster.functions = makeClusterFunctionsMulticore(ncpus = 2)
-  addProblem(name   = "pem-vs-pam", fun = sim_wrapper)
-  addAlgorithm(name = "pem-vs-pam", fun = pam_wrapper)
-
-  algo_df <- tidyr::crossing(
-    cut = c(NA, "fine", "rough"),
-    mod_type = c("pem", "pam"))
-
-  addExperiments(algo.design  = list("pem-vs-pam" = algo_df), repls = 10)
-  submitJobs()
-  waitForJobs()
-}
-
-reg     <- loadRegistry("C:/Users/ra56yaf/Desktop/Projects/StaBLab/Survival Analysis/survival_kidneyFunction/msm_kidneyFunction/output/sim-pem-vs-pam-registry", writeable = TRUE)
-ids_pam <- findExperiments(prob.name="pem-vs-pam", algo.name="pem-vs-pam")
-pars    <- unwrap(getJobPars()) %>% as_tibble()
-res     <- reduceResultsDataTable(ids=findDone(ids_pam)) %>%
-  as_tibble() %>%
-  tidyr::unnest() %>%
-  left_join(pars) %>%
-  mutate(cut = case_when(is.na(cut) ~ "default", TRUE ~ cut))
-
-res %>%
-  mutate(
-    sq_error = (truth - hazard)^2,
-    covered = (truth >= ci_lower) & (truth <= ci_upper)) %>%
-  group_by(job.id, mod_type, cut) %>%
-  summarize(
-    RMSE = sqrt(mean(sq_error)),
-    coverage = mean(covered)) %>%
-  group_by(mod_type, cut) %>%
-  summarize(
-    RMSE     = mean(RMSE),
-    coverage = mean(coverage))
-
-ggplot(res, aes(x=tend, y = hazard)) +
-  geom_step(aes(group = job.id), alpha = 0.3) +
-  geom_line(aes(y = truth, col = "truth"), lwd = 2) +
-  facet_grid(cut ~ mod_type) +
-  coord_cartesian(ylim=c(-5, 0)) +
-  geom_smooth(aes(col="average estimate"), method="gam", formula = y ~ s(x),
-    se=FALSE) +
-  scale_color_brewer("", palette = "Dark2") +
-  xlab("time")
